8a692936665945146d207d16759c209c34cf347e
Avinash Poojary <avinashpoojary651@gmail.com>
2025-09-16 16:08:04 +0530

    route update and port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
